0943b0ac93653de9e6e203e7c24066c9a419ea75
francistoleedo <francisntoledo@hotmail.com>
2025-07-18 18:00:11 -0300

    Ajustando init

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import Status from "./router/status-router";
-import UserRoute from "./router/user-route";
+// import UserRoute from "./router/user-route";
 const server = express();
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use("/api", Status);
-server.use("/api", UserRoute);
+// server.use("/api", UserRoute);
 
 export { server };
